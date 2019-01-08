8b9f492ed652d19cf16f8237c2dd5cd5a838e691
krez <fela.gonzalezm@gmail.com>
2019-01-08 11:28:58 -0300

    Proyecto inicializado

---
+++ src/main/java/com/example/demo/models/entity/Customer.java
@@ -0,0 +1,11 @@
+package com.example.demo.models.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    
+}
